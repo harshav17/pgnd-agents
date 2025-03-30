55d2e5d4c7e86dc9adaad0e555893bbd7f5a9e07
harsha <harshav17@gmail.com>
2025-03-30 16:42:59 -0700

    test pydantic contrib

---
+++ backend/city_location_agent.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+from pydantic_ai import Agent
+from devtools import debug
+
+class CityLocation(BaseModel):
+    city: str
+    country: str
+
+
+agent = Agent('openai:gpt-4o', result_type=CityLocation)
+result = agent.run_sync('Where were the olympics held in 2012?')
+debug(result.all_messages())
+print(result.data)
+#> city='London' country='United Kingdom'
+print(result.usage())
+"""
+Usage(requests=1, request_tokens=57, response_tokens=8, total_tokens=65, details=None)
+"""
