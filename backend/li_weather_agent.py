160f6948c1647d0cdb94dcab9eabd6dacc5d4871
harsha <harshav17@gmail.com>
2025-03-31 21:55:21 -0700

    stream llama

--- backend/li_weather_agent.py
+++ backend/li_weather_agent.py
@@ -1,6 +1,7 @@
 from llama_index.llms.openai import OpenAI
 from llama_index.core.agent.workflow import FunctionAgent
 import asyncio
+from llama_index.core.agent.workflow import ToolCallResult
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -15,16 +16,26 @@ def add(a: float, b: float) -> float:
     return a + b
 
 llm = OpenAI(model="gpt-4o-mini")
-workflow = FunctionAgent(
+agent = FunctionAgent(
     tools=[multiply, add],
     llm=llm,
     system_prompt="You are an agent that can perform basic mathematical operations using tools.",
 )
 
-async def main():
-    response = await workflow.run(user_msg="What is 20+(2*4)?")
-    print(response)
 
+async def run_agent_verbose(query: str):
+    handler = agent.run(query)
+    async for event in handler.stream_events():
+        if isinstance(event, ToolCallResult):
+            print(
+                f"Called tool {event.tool_name} with args {event.tool_kwargs}\nGot result: {event.tool_output}"
+            )
+
+    return await handler
+
+async def main():
+    response = await run_agent_verbose("What is (121 + 2) * 5?")
+    print(str(response))
 
 if __name__ == "__main__":
     asyncio.run(main())
